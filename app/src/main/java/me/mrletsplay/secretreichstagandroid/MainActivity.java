fc6b7f61e028abfcc0099609e70ef764e7001f88
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2021-11-27 21:26:26 +0100

    Remove unnecessary RequiresApi

--- app/src/main/java/me/mrletsplay/secretreichstagandroid/MainActivity.java
+++ app/src/main/java/me/mrletsplay/secretreichstagandroid/MainActivity.java
@@ -70,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
 
 	private ExtendedRoomSettings roomSettings;
 
-	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
